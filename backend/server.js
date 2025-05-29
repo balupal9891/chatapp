d6ecd6bc94c1d40f963fcd8884c245e309738c0f
Balu Pal <balupal9891@gmail.com>
2025-05-29 17:31:07 +0530

    bug resolved

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import authRoutes from './routes/auth.route.js';
 import connectToMongo from './db/connectToMongo.js';
 import messageRoutes from './routes/message.route.js'
@@ -8,26 +9,29 @@ import cookieParser from 'cookie-parser';
 import { server,app } from './socket/socket.js';
 dotenv.config();
 
-
+const __dirname = path.resolve();
 // const app = express();
-
-app.get('/', (req, res)=>{
-    res.send("hello world");
-})
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
 
-
-
-
+// app.get('/', (req, res)=>{
+//     res.send("hello world");
+// })
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-server.listen(process.env.PORT || 5000, ()=>{
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+
+server.listen(PORT || 5000 , ()=>{
     connectToMongo();
     console.log("server is running on 5000");
 });
